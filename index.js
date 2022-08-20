5ef531e53fbd6f47ac4ce782eb8411c963866dfa
LAPTOP-747E5JBF\matep <matepranay11@gmail.com>
2022-08-21 00:20:02 +0530

    app update

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const port = process.env.PORT || 5000;
 if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'){
     app.use(express.static('client/build'))
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'/client/build/index.html'))
     })
 }
 
