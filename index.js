83a4e6c7c1f6428f3543cd4b7d83945c650b1240
LAPTOP-747E5JBF\matep <matepranay11@gmail.com>
2022-08-21 13:52:21 +0530

    a

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ const uri = "mongodb+srv://Pranay:KEYYs1PXzpmdEspb@cluster0.kx27fbs.mongodb.net/
 const port = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'){
-    app.use(express.static('split_app/build'))
+    app.use(express.static("split_app/build"))
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'/split_app/build/index.html'))
+        res.sendFile(path.resolve(__dirname,'split_app','build','index.html'))
     })
 }
 
